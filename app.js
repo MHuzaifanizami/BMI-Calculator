c03e1106f47ac4c1d4dabb43e2597aff81e7e904
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2024-07-03 23:18:22 +0500

    file updated

---
+++ app.js
@@ -0,0 +1,30 @@
+var form = document.querySelector("form");
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    var height = parseInt(document.getElementById("height").value);
+    var wieght = parseInt(document.getElementById("weight").value);
+    var result = document.getElementById("result");
+    var suggest = document.getElementById("suggest");
+
+    if(height == "" || height < 0 || isNaN(height)){
+        result.innerHTML = "Please Enter a valid Height"
+    }
+    else if(wieght == "" || wieght < 0 || isNaN(wieght)){
+        result.innerHTML = "Please Enter a valid weight"
+
+    }else{
+        var bmi = (wieght / ((height * height) / 10000)).toFixed(2);
+
+        result.innerHTML = `${bmi}`
+    }
+
+   if(bmi < 18.6){
+    suggest.innerHTML = `Under Weight`
+   }else if(bmi < 24.9){
+    suggest.innerHTML = `Normal Range`
+   }else if(bmi > 24.9){
+    suggest.innerHTML = `Over Weight`
+   }
+})
